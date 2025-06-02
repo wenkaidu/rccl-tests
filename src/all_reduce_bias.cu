08517571cf8d918d9ce31629539544cd034fa40d
Wenkai Du <wenkai.du@amd.com>
2025-06-02 18:38:29 -0500

    Add all_reduce_bias_perf to support All Reduce with Bias

--- src/all_reduce_bias.cu
+++ src/all_reduce_bias.cu
@@ -27,7 +27,9 @@ testResult_t AllReduceInitData(struct threadArgs* args, ncclDataType_t type, ncc
     CUDACHECK(cudaMemset(args->recvbuffs[i], 0, args->expectedBytes));
     void* data = in_place ? args->recvbuffs[i] : args->sendbuffs[i];
     TESTCHECK(InitData(data, sendcount, 0, type, op, rep, nranks, rank));
+    TESTCHECK(InitData(args->bias[i], sendcount, 0, type, op, rep+0x12345678, nranks, rank));
     TESTCHECK(InitDataReduce(args->expected[i], recvcount, 0, type, op, rep, nranks));
+    TESTCHECK(InitDataApplyBias(args->expected[i], args->bias[i], recvcount, 0, type, op));
     CUDACHECK(cudaDeviceSynchronize());
   }
   return testSuccess;
@@ -42,7 +44,7 @@ void AllReduceGetBw(size_t count, int typesize, double sec, double* algBw, doubl
 }
 
 testResult_t AllReduceRunColl(void* sendbuff, void* recvbuff, size_t count, ncclDataType_t type, ncclRedOp_t op, int root, ncclComm_t comm, cudaStream_t stream, void* bias = nullptr) {
-  NCCLCHECK(ncclAllReduceWithBias(sendbuff, recvbuff, count, type, op, comm, stream, nullptr));
+  NCCLCHECK(ncclAllReduceWithBias(sendbuff, recvbuff, count, type, op, comm, stream, bias));
   return testSuccess;
 }
 
@@ -65,6 +67,7 @@ testResult_t AllReduceRunTest(struct threadArgs* args, int root, ncclDataType_t 
   ncclRedOp_t *run_ops;
   const char **run_typenames, **run_opnames;
   int type_count, op_count;
+
   if ((int)type != -1) {
     type_count = 1;
     run_types = &type;
@@ -88,8 +91,8 @@ testResult_t AllReduceRunTest(struct threadArgs* args, int root, ncclDataType_t 
   for (int i=0; i<type_count; i++) {
     for (int j=0; j<op_count; j++) {
 #if defined(RCCL_FLOAT8)
-  if((run_types[i] == ncclFloat8e4m3 || run_types[i] == ncclFloat8e5m2) && (run_ops[j] == ncclProd || run_ops[j] == ncclAvg || strcmp(run_opnames[j],"mulsum") == 0))
-    continue;
+      if((run_types[i] == ncclFloat8e4m3 || run_types[i] == ncclFloat8e5m2) && (run_ops[j] == ncclProd || run_ops[j] == ncclAvg || strcmp(run_opnames[j],"mulsum") == 0))
+      continue;
 #endif
       TESTCHECK(TimeTest(args, run_types[i], run_typenames[i], run_ops[j], run_opnames[j], -1));
     }
